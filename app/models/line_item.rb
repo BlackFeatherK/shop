48605e80ee1d9dc865c1eba4fb9dddb3918d830b
BlackFeather <she3354248@yahoo.com.tw>
2016-12-11 12:55:45 +0800

    add product , order , line_item , cart model

---
+++ app/models/line_item.rb
@@ -0,0 +1,4 @@
+class LineItem < ApplicationRecord
+	belongs_to :cart , :optional => true
+	belongs_to :order , :optional => true
+end
---
+++ db/migrate/20161211033943_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+    	t.integer :user_id , :index => true
+    	t.string :email
+    	t.string :receiver_name
+    	t.string :cellphone
+    	t.string :address
+
+    	t.integer :amout
+    	
+      t.timestamps
+    end
+  end
+end
